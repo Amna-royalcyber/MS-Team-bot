8940a5a851085451fdb27fb5800eb60b01c79cd5
Amna-royalcyber <amna.zahid@royalcyber.com>
2026-04-16 17:24:32 +0500

    Revert "commit"
    This reverts commit 63ead5e46c89660d9442e265ace33def36b0b5ae.

--- TranscriptHub.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.SignalR;
-
-namespace TeamsMediaBot;
-
-public sealed class TranscriptHub : Hub
-{
-    // Marker hub for server-driven transcript and reconciliation events.
-    // Clients should subscribe to: transcript, transcript-update, identity-resolved, transcript-retroactive-update, roster.
-}
